876a7465d2a46550b6b017907e6a0d381d1d6ca5
Maria Martins <2230770@my.ipleiria.pt>
2025-12-04 15:57:10 +0000

    Adicionei o form e os NuGet packages

---
+++ ApplicationB/ApplicationB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace ApplicationB
+{
+    public partial class ApplicaionB : Form
+    {
+        public ApplicaionB()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicaionB_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
